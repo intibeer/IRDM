f73dfedeee6cb2574033fe3c0bb4ed5c683e67ae
mayya-bondarevskaya <mayya.bondarevskaya@gmail.com>
2017-04-20 22:41:45 +0100

    add random forests commands for training models

---
+++ commands/RandomForests.sh
@@ -0,0 +1,39 @@
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 1 -tree 1 -leaf 100 -save random_forest_bag1_tree1_leaf100.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 10 -tree 1 -leaf 100 -save random_forest_bag10_tree1_leaf100.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 20 -tree 1 -leaf 100 -save random_forest_bag20_tree1_leaf100.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 100 -tree 1 -leaf 100 -save random_forest_bag100_tree1_leaf100.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 150 -tree 1 -leaf 100 -save random_forest_bag150_tree1_leaf100.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 200 -tree 1 -leaf 100 -save random_forest_bag200_tree1_leaf100.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 250 -tree 1 -leaf 100 -save random_forest_bag250_tree1_leaf100.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 300 -tree 1 -leaf 100 -save random_forest_bag300_tree1_leaf100.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 350 -tree 1 -leaf 100 -save random_forest_bag350_tree1_leaf100.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 400 -tree 1 -leaf 100 -save random_forest_bag400_tree1_leaf100.txt
+
+
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 100 -tree 1 -leaf 10 -save random_forest_bag100_tree1_leaf10.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 100 -tree 1 -leaf 25 -save random_forest_bag100_tree1_leaf25.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 100 -tree 1 -leaf 50 -save random_forest_bag100_tree1_leaf50.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 100 -tree 1 -leaf 75 -save random_forest_bag100_tree1_leaf75.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 100 -tree 1 -leaf 100 -save random_forest_bag100_tree1_leaf100.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 100 -tree 1 -leaf 125 -save random_forest_bag100_tree1_leaf125.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 100 -tree 1 -leaf 150 -save random_forest_bag100_tree1_leaf150.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 100 -tree 1 -leaf 175 -save random_forest_bag100_tree1_leaf175.txt
+
+java -jar RankLib.jar -train train.txt -test test.txt -validate vali.txt -ranker 8 -bag 100 -tree 1 -leaf 200 -save random_forest_bag100_tree1_leaf200.txt
